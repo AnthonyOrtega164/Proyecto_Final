be87ec6d66025c333d247dddb352a48aa971198e
djmer <diego-stainl@hotmail.com>
2018-08-18 22:19:56 -0500

    modelo de conexion persona

---
+++ app/Models/Persona.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Description of Persona
+ *
+ * @author Usuario
+ */
+class Persona extends Model {
+    
+    protected  $table = 'persona';
+    protected  $primaryKey = 'id_persona';
+    public $timestamps = false;
+    protected $fillable = ['correo_persona','nombre_persona','telefono_persona', 'foto_persona'];
+    protected $guarded =['id_persona'];
+    
+    
+    }
