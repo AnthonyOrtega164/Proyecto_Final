303ba50e92a7afe8e163e99bf4905fef78e5ecd6
AnthonyOrtega164 <anthonyortega164@gmail.com>
2018-09-04 17:35:35 -0500

    documentaciones pequeñas

--- MiAdopcion/app/src/main/java/com/adom/miadopcion/modelos/Imagen.java
+++ MiAdopcion/app/src/main/java/com/adom/miadopcion/modelos/Imagen.java
@@ -1,6 +1,9 @@
 package com.adom.miadopcion.modelos;
 
 public class Imagen {
+    /**
+     *@param variables tipo String datos de imagen ,para conexion y consumo con fireBase
+     */
     public String id_imagen;
     public String id_publicacion;
     public String ruta_imagen;
